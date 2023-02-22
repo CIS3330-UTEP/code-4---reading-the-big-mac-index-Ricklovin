ab8f343027d24e2b412f53431e813575f3d68f21
Ricklovin <123012156+Ricklovin@users.noreply.github.com>
2023-02-22 16:36:06 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -8,32 +8,39 @@ def get_big_mac_price_by_year(year,country_code):
     query = f"year = {year}, and iso_a3 =='{country_code}'"
     big_mac_price_by_year = df.query(query)
 
-    return big_mac_price_by_year 
-     # Number round 2
+    return big_mac_price_by_year.round(2)    #number 
+    
 def get_big_mac_price_by_country(country_code):
     df = pd.read_csv(big_mac_file)
 
     query = f"iso_a3 == '{country_code}'"
-
     big_mac_price_by_country = df.query(query)
 
-    return big_mac_price_by_country 
-    # number round 2
+    return big_mac_price_by_country.round(2)    #number 
+    
 def get_the_cheapest_big_mac_price_by_year(year):
     df = pd.read_csv(big_mac_file)
 
-    query = f""
-
+    query = f"year == {year}"
     the_cheapest_big_mac_price_by_year =df.query(query)
 
-    return the_cheapest_big_mac_price_by_year 
+    return the_cheapest_big_mac_price_by_year.round(2)
     # message
 def get_the_most_expensive_big_mac_price_by_year(year):
     df = pd.read_csv(big_mac_file)
     query = f""
     the_most_expensive_big_mac_price_by_year = df.query(query)
 
-    return the_most_expensive_big_mac_price_by_year
-    # mess
+    return the_most_expensive_big_mac_price_by_year.round(2)
 if __name__ == "__main__":
-    print(get_big_mac_price_by_year)
+
+    year = int(input("Year desired: "))
+    country_code = input("Enter country code: ")
+
+    big_mac_price_by_year = get_big_mac_price_by_year(year,country_code)
+    print(f"{big_mac_price_by_year}")
+
+    big_mac_price_by_country = get_big_mac_price_by_country(country_code)
+    print(f"{big_mac_price_by_country}")
+
+  
