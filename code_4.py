64a476980ce4e8eb7dd8e89fdfaba083886c7d8b
Ricklovin <123012156+Ricklovin@users.noreply.github.com>
2023-02-22 12:39:14 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,19 +1,39 @@
-import csv
+
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
-cvs_reader = csv.reader(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    df = pd.read_csv(big_mac_file)
 
+    query = f"year = {year}, and iso_a3 =='{country_code}'"
+    big_mac_price_by_year = df.query(query)
+
+    return big_mac_price_by_year 
+     # Number round 2
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    df = pd.read_csv(big_mac_file)
 
+    query = f"iso_a3 == '{country_code}'"
+
+    big_mac_price_by_country = df.query(query)
+
+    return big_mac_price_by_country 
+    # number round 2
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.read_csv(big_mac_file)
 
+    query = f""
+
+    the_cheapest_big_mac_price_by_year =df.query(query)
+
+    return the_cheapest_big_mac_price_by_year 
+    # message
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.read_csv(big_mac_file)
+    query = f""
+    the_most_expensive_big_mac_price_by_year = df.query(query)
 
+    return the_most_expensive_big_mac_price_by_year
+    # mess
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    print(get_big_mac_price_by_year)
