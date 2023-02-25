d27bf5f0c7f4a7dfd506b01f617f7e49caeb605a
Ricklovin <123012156+Ricklovin@users.noreply.github.com>
2023-02-24 23:24:08 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -10,48 +10,22 @@ def get_big_mac_price_by_year(year,country_code):
     big_mac_price_by_year = df.query(querys)                                       
 
     return big_mac_price_by_year.round(2)    #number 
-    
 
 def get_big_mac_price_by_country(country_code):
     df = pd.read_csv(big_mac_file)
 
-    query = df.iso_a3 == {country_code}
+    query = f"iso_a3 == {country_code}"
     big_mac_price_by_country = df.query(query)
 
+    mean_price = big_mac_price_by_country['dollar_price'].idxmean
 
+    country_code = df.loc[mean_price]
 
+    mean_country_price = f"{country_code['dollar_price'].round(2)}"
 
-
-    return big_mac_price_by_country.round(2)    #number 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return mean_country_price    
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    df = pd.read_csv(big_mac_file)
-
     df = pd.read_csv(big_mac_file)
 
     querys = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
@@ -65,11 +39,6 @@ def get_the_cheapest_big_mac_price_by_year(year):
     min_big_mac = f"{country_code['name']}({country_code['iso_a3']}): ${country_code['dollar_price'].round(2)}"
     
     return min_big_mac
-
-
-
-
-
 
 
 def get_the_most_expensive_big_mac_price_by_year(year):
@@ -88,19 +57,6 @@ def get_the_most_expensive_big_mac_price_by_year(year):
 
     return max_big_mac
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
 
     year = input("The year desired:  ")
@@ -111,6 +67,7 @@ if __name__ == "__main__":
 
     print(get_the_cheapest_big_mac_price_by_year(year))
 
+    print(get_big_mac_price_by_country(country_code))
 
 
 
