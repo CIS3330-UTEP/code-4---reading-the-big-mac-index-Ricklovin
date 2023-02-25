0219a927e2123ce6a016da9e6c89852999b02829
Ricklovin <123012156+Ricklovin@users.noreply.github.com>
2023-02-24 17:28:28 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,46 +1,91 @@
-
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
     df = pd.read_csv(big_mac_file)
 
-    query = f"year = {year}, and iso_a3 =='{country_code}'"
+    query = df.loc[(df['date'] == {year}) and (df.iso_a3 == {country_code}.upper())]
+
+
+
+
+
     big_mac_price_by_year = df.query(query)
 
     return big_mac_price_by_year.round(2)    #number 
     
+
+
+
+
 def get_big_mac_price_by_country(country_code):
     df = pd.read_csv(big_mac_file)
 
-    query = f"iso_a3 == '{country_code}'"
+    query = df.iso_a3 == {country_code}
     big_mac_price_by_country = df.query(query)
+
+
+
+
 
     return big_mac_price_by_country.round(2)    #number 
     
+
+
+
 def get_the_cheapest_big_mac_price_by_year(year):
     df = pd.read_csv(big_mac_file)
+    
 
-    query = f"year == {year}"
-    the_cheapest_big_mac_price_by_year =df.query(query)
+   # the_cheapest_big_mac_price_by_year = df.query(query)
 
-    return the_cheapest_big_mac_price_by_year.round(2)
+
+
+#
+  #  return the_cheapest_big_mac_price_by_year.round(2)
+    
+    
+    
+    
+    
     # message
 def get_the_most_expensive_big_mac_price_by_year(year):
     df = pd.read_csv(big_mac_file)
-    query = f""
+    query = f"date == {year}"
     the_most_expensive_big_mac_price_by_year = df.query(query)
 
-    return the_most_expensive_big_mac_price_by_year.round(2)
-if __name__ == "__main__":
-
-    year = int(input("Year desired: "))
-    country_code = input("Enter country code: ")
-
-    big_mac_price_by_year = get_big_mac_price_by_year(year,country_code)
-    print(f"{big_mac_price_by_year}")
-
-    big_mac_price_by_country = get_big_mac_price_by_country(country_code)
-    print(f"{big_mac_price_by_country}")
-
   
+  
+  
+  
+  
+    return the_most_expensive_big_mac_price_by_year.round(2)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(big_mac_file)
+
+    print(df.columns)
+
+    print(df.iso_a3)
+
+
+    year = (input("Year desired: "))
+    country_code = str(input("Country Code: ").lower())
+
+
+
+
+    print(get_big_mac_price_by_country(country_code))
+   
+    
+
+   
